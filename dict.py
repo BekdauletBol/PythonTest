40dbbd7b3acec0f7fdfbf279453f876ebf913743
BekdauletBol <bekdauletmakemusic@gmail.com>
2026-03-09 15:29:20 +0500

    update dict

--- dict.py
+++ dict.py
@@ -6,6 +6,26 @@ students = {"name" : "Bekdaulet", "age" : 25, and etc}
 
 in this case name and age are keys and Bekdaulet and 25 are values
 
-"""
+and if i wanna to add new item in our dictionary
+i just write
+
+name of var["New Item] = "our value"
+for example
+
+i have dict houses
 
 houses = {"Harry":"Gryffindor", "Draco": "Slytherin"}
+and i just write
+houses["Bekdaulet"] = "Gryffindor"
+
+
+"""
+
+houses = {"Harry":"Gryffindor", "Draco": "Slytherin","Yernazar": "Aitushny"}
+houses["Hermione"] = "Gryffindor"
+houses["Dimash"] = "Aitushny"
+
+print(houses["Harry"])
+print(houses["Draco"])
+print(houses["Hermione"])
+print(houses["Dimash"])
